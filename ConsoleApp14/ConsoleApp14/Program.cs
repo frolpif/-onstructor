1c92abda72e3d4b6162024f4bfdafd2ceb68ebed
frolpif <115400244+frolpif@users.noreply.github.com>
2023-06-15 19:41:45 +0500

    Add files via upload

---
+++ ConsoleApp14/ConsoleApp14/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, мир!");
+            MyClass instance = new MyClass();
+           // Console.WriteLine(MyClass.StaticField);
+        }
+    }
+    class MyClass
+    {
+        public static float StaticField;
+        static MyClass()
+        {
+            StaticField = 10;
+            Console.WriteLine("Статический конструктор.");
+        }
+        public MyClass()
+        {
+            Console.WriteLine("Обычный конструктор.");
+        }
+    }
+}
